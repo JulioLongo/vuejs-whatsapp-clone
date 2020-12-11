f39713b1f824acd23482d40b60f88f4b73bfec7f
Julio <juliolongocontato@gmail.com>
2020-12-11 18:32:22 -0300

    changes

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <ul>
+      <li>teste</li>
+      <li>teste2</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
